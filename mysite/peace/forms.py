041ce2174815382b2631cd3f4a2d4862c64d061d
kimchaeyoung <codud-45@hanmail.net>
2019-03-03 21:01:32 +0900

    server redundancy check

--- mysite/peace/forms.py
+++ mysite/peace/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django import forms
 from .models import Applicant
 import re
+import subprocess
 
 class PostForm(ModelForm):
 
@@ -14,6 +15,7 @@ class PostForm(ModelForm):
         'usage', 'username', 'agreement']
 
     def clean_username(self):
+
         username = self.cleaned_data['username']
         special_case = re.findall("[^a-zA-Z0-9]",username)
         if special_case:
@@ -21,4 +23,8 @@ class PostForm(ModelForm):
 
         if Applicant.objects.filter(username=username).exists():
             raise forms.ValidationError("The username is already in use")
+
+        if subprocess.call(['grep',username,'/etc/passwd']) == 0:
+            raise forms.ValidationError("The username is already in use")
+
         return username
